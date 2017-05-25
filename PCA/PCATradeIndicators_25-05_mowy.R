91ce479c3490a98a7dd4a402540d096fa4ca6b11
Stefano Moawad <mowythebest@Mowys-Air.(none)>
2017-05-25 18:40:25 +0200

    Pca eco and pca trade

--- PCA/PCATradeIndicators_25-05_mowy.R
+++ PCA/PCATradeIndicators_25-05_mowy.R
@@ -38,7 +38,8 @@ library(dplyr)
 # "Service exports (BoP, current US$)",
 # "Service imports (BoP, current US$)",
 # "Trade in services (% of GDP)"
-#TOGLIERE ULTIME TRE MIGLIORA VAR SPIEGATA però è analisi diversa
+# TOGLIERE ULTIME TRE MIGLIORA VAR SPIEGATA però è analisi diversa
+# ha anche senso lavorare solo con indicatori contrasto imp exp
 
 # I want to keep the fullest M indicators
 M <- 400
@@ -66,9 +67,9 @@ myInd <- c("Export value index (2000 = 100)",
            "Time to export (days)",
            "Time to import (days)",
            "Trade (% of GDP)")
-           "Service exports (BoP, current US$)",
-           "Service imports (BoP, current US$)",
-           "Trade in services (% of GDP)")
+           # "Service exports (BoP, current US$)",
+           # "Service imports (BoP, current US$)",
+           # "Trade in services (% of GDP)")
 
 # Set the year:
 myYear <- c(2010)
@@ -81,7 +82,7 @@ TradeMatrix <- getCntInd(TradeDF, myYear, dropNA = T, showCnt = T)
 colnames(TradeMatrix) <- c(
 "Exp ind",
 "Imp ind",
-#"Merch trade",
+"Merch trade",
 "Cost exp",
 "Cost imp",
 "Doc exp",
@@ -90,11 +91,11 @@ colnames(TradeMatrix) <- c(
 "Imp goods&services",
 "Time exp",
 "Time imp",
-"Trade",
-"Exp ser",
-"imp serv",
-"trade serv")
-#EcoIndMatrix <- find_outlier(EcoIndMatrix, remove = T) 
+"Trade")
+# "Exp ser",
+# "imp serv",
+# "trade serv")
+#TradeMatrix <- find_outlier(TradeMatrix, remove = T) 
 # Remark: THERE ARE OUTLIERS: working without them means a 72,21% covered by the first compo, the second is 
 #                             almost the same of with outilers. The loadings are the same!
 #                             working with them means 64,21% covered by the first, the others the same of no
@@ -140,12 +141,12 @@ x11()
 par(mar = c(1,4,0,2), mfrow = c(4,1))
 for(i in 1:4) barplot(TradeLoad[,i],las=2, ylim = c(-1, 1))
 
-# Interpretation of the loadings:
-# First PC: GDP + foreign indicator VS inflations
-# Second PC: Countries with high GDP and Inflation, but trustuble for foregn investments
-# Third PC: High Gdp per capita
-
-# Explained variance
+# Interpretation of the loadings 15 compo:
+# + difificle ma possibile
+# Interpretation of the loadings 12 compo:
+# - difficile
+# Interpretation of the loadings - anche le due di trade:
+# la più sensata di interpr
 x11()
 layout(matrix(c(2,3,1,3),2,byrow=T))
 barplot(TradePCA$sdev^2, las=2, main='Principal Components', ylim=c(0,4), ylab='Variances')
@@ -157,7 +158,7 @@ box()
 axis(2,at=0:10/10,labels=0:10/10)
 axis(1,at=1:ncol(TradeMatrixStd),labels=1:ncol(TradeMatrixStd),las=2)
 
-# The first two PC explains more than 82.87% of the total variability.
+# The first four PC explains more than 80% of the total variability.
 
 # Scores: repr of the original data in the space PC1 PC2
 TradeScores <- TradePCA$scores  
@@ -171,16 +172,7 @@ boxplot(TradeScores, las=2, col='red', main='Principal components')
 x11()
 biplot(TradePCA, scale=0, cex=.7)
 
-# INTERPRETAZIONE: the more on the right the more inflation
-#                  the more on the left the more strong and growing GDP and foregn investments
-#                  the lower the more higher gdp and inflation but still trustable
-# In alto a sx scandinavi e svizzera e hong kong 
-# in bassino a super sx Paesi con economie più avanzate (ci sarebbe super a sx USA) (USA China Canada Australia, jappone Germania)
-# super in basso a dx fortix inflaz
-# al centro in basso paesi con alto gdp ma economia debole per cittadini(alta inflaz): Russia brasile india
-# al centro poco sopra paesi altix pil percapite middle east
-# al centro più in alto leggerm verso dx quelle più debole : asia e africa e più vado a dx più economie complicate ciclo inflation
-# al centro più in alto ma verso sx paesi europei e altri tipo israele che sono forti ma senza eccellere troppo
+#Interpretaz per clustering
 # x11()
 # PCbiplot(prcomp(scale(TeleMatrix)))
 
