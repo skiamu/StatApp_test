1d0f19da7c8281b4c0946c8a06b21f86c15671f3
Stefano Moawad <mowythebest@Mowys-Air.(none)>
2017-06-15 19:16:47 +0200

    FdaPredict

--- Cluster/fdaPred.R
+++ Cluster/fdaPred.R
@@ -1,9 +1,10 @@
-# fda is a function that perform the Fisher linear discriminant analysis:
+# fdaPred is a function that perform predict a cluster by Fisher linear discriminant analysis:
 # we assume: 1) same cost of misclassifying, 
 #            2) same covariance matrix, 
 #            3) prior prob proportional to the number of elements
+# Warning: pass the data standardized
 
-fda <- function(data, clusters, clustersLabels, numComp, numVar){
+fdaPred <- function(data, clusters, clustersLabels, numComp, numVar, predDataStd){
   
   # -- INPUT: 
   #    - data          : dataframe on which perform the discrimination
@@ -11,8 +12,9 @@ fda <- function(data, clusters, clustersLabels, numComp, numVar){
   #    - clustersLabels: labels of the clusters [vector of strings]
   #    - numComp       : number of fisher components  
   #    - numVar        : number of variables
+  #    - predDataStd   : obs to discriminate [standardized]
   # -- OUTPUT:
-  #    - a : matrix in which the cloumns are the components
+  #    - cluster : cluster for the observation to discriminate
   
   # ingridient to compute the Matrix Sp^(-1/2)
   m <-  colMeans(data)
@@ -22,12 +24,13 @@ fda <- function(data, clusters, clustersLabels, numComp, numVar){
   nTot <- 0
   Sp <- 0
   B <- 0
+  mcol <- array(0, c(g, p))
   for(i in 1:g){
     indeces <- which(clusters==clustersLabels[i])
     n <- length(indeces)
     nTot=nTot+n
     cov  <-  cov(data[indeces,])
-    mcol <- colMeans(data[indeces,])
+    mcol[i,] <- colMeans(data[indeces,])
     Sp = Sp + (n-1)*cov
     B= B + n * cbind(mcol - m) %*% rbind(mcol - m)
   }
@@ -44,5 +47,13 @@ fda <- function(data, clusters, clustersLabels, numComp, numVar){
   # computation of the Fisher components
   a <- array(0, c(p,numComp))
   for(i in 1:numComp){a[,i] <- invSp %*% spec.dec$vec[,i]}
-  return(a)
+  
+  # discrimination
+  cc<- mcol%*%a
+  ccPred=predDataStd%*%a
+  dist<-array(1,g)
+  for(i in 1:g){dist[i]=sqrt(sum((ccPred-cc[i,])^2))}
+  # assign to the nearest mean
+  cluster=which.min(dist)
+  return(cluster)
 }
